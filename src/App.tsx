9bdbe1ac6de5780cb4d3e6a9738b7d5c0d270c74
Jonathas Sardinha <jonathas.sardinha@gmail.com>
2024-09-19 22:55:23 -0300

    Add base website

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav className="absolute top-0 left-0 flex items-center justify-between w-full px-20 pt-4">
+        <a className="text-white font-normal">Function</a>
+        <a className="text-white font-normal">Collection</a>
+        <a className="text-white font-normal">Configuration</a>
+        <a className="text-white font-normal">About</a>
+      </nav>
+      <main className="flex flex-col grow">
+        <section
+          style={{
+            backgroundImage: "url('/hero-image.png')",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+          className="h-screen pt-32 pl-20"
+        >
+          <h1>
+            <span className="block">Your workspace.</span>
+            <span className="block">Reinvented.</span>
+          </h1>
+        </section>
+        <section className="px-20 py-12 bg-gray-100">
+          <h2 className="text-3xl font-semibold text-black">Our products.</h2>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
